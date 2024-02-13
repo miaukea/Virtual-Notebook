07b798213f1b2c087227b69da207b0cff97134f7
Melody <meliaukea@gmail.com>
2024-02-12 21:29:32 -0500

    Got website working on server and fixed code for server.js

--- Develop/server.js
+++ Develop/server.js
@@ -2,11 +2,26 @@
 const express = require('express')
 const path = require('path')
 const fs = require('fs')
+
 const PORT = 3001
+
 const app = express()
 
-// Setting up middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/api', api)
 app.use(express.static('public'))
+app.use(express.json())
+
+const api = require('./routes/index')
+
+app.use('/api', api)
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.listen(PORT, () =>
+    console.log('App listening at http://localhost:${PORT}')
+)
